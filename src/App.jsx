f2ce3e1402b7ea0375793c596bb47b7fbbb13413
alexmilst <icarusnyc94@gmail.com>
2026-07-27 08:38:57 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -528,7 +528,12 @@ export default function CasaCastellanV2() {
           <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
             <div style={{ height: 10 }} aria-hidden="true" />
             <div style={{ flex: 1, display: "flex", alignItems: "flex-start", justifyContent: "flex-end" }}>
-              <img src={LOGO} alt="Casa Castellan" className="cc-hero-logo" style={{ width: "100%", height: "100%", objectFit: "contain", objectPosition: "top right" }} />
+              <img
+                src={LOGO}
+                alt="Casa Castellan"
+                className="cc-hero-logo"
+                style={{ width: "100%", height: "100%", objectFit: "contain", objectPosition: "top right", transform: "translateX(40px)" }}
+              />
             </div>
             <div style={{ height: 20 }} aria-hidden="true" />
           </div>
